a4db1f0cd481cae76490d1feddca0c1d43410ed5
vcursi-healthbit <vinicius.cursi@healthbit.com.br>
2022-02-14 09:36:09 -0300

    Primeiro Commit para o repositírio
        Fork do repo: https://github.com/mentoriaiac/iac-modulo-github.git
    
        *feat: Adicionado variavel de permisão de repositorio
    
        *feat: Adicionado variável de role para membro do time

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "github_team_membership" "team_membership" {
   for_each = toset(var.team_members)
   team_id  = data.github_team.team.id
   username = each.value
-  role     = "member"
+  role     = var.role
 }
 
 
@@ -22,5 +22,5 @@ resource "github_team_repository" "manage_access_repository" {
   for_each   = toset(data.github_repositories.repositories.names[*])
   team_id    = data.github_team.team.id
   repository = each.key
-  permission = "admin"
+  permission = var.permission
 }
